1067449aa03171cc1c3c800463d18fa2bb11e256
BrackListed <cameta8900@gmail.com>
2026-05-28 19:48:23 +0800

    Can now add tasks

--- backend/index.ts
+++ backend/index.ts
@@ -5,20 +5,23 @@ import { Pool } from "pg"
 
 //npx nodemon --exec ts-node index.ts
 
-
 const pool = new Pool({ connectionString: process.env.DATABASE_URL})
-let tasks: any[] = []
+
 const app = express()
 
 
 app.use(express.json())
 app.use(cors())
 
-// app.post('/data', async (req, res) => {
-//     const result = await pool.query("SELECT name FROM tasks")
-//     tasks.push(result)
-//     res.json(result)
-// })
+app.delete("/data/:id", async (req, res) => {
+    await pool.query("DELETE FROM tasks WHERE id = $1", [req.params.id])
+    res.json({message: "Deleted"})
+})
+
+app.post("/data", async (req, res) => {
+    await pool.query("INSERT into tasks(name, done) VALUES($1, $2)", [req.body.name, false])
+    res.json([{message: "Task added!"}])
+})
 
 app.get("/data", async (req, res) => {
     const result = await pool.query("SELECT * FROM tasks")
--- frontend/src/pages/Tasks.tsx
+++ frontend/src/pages/Tasks.tsx
@@ -8,6 +8,7 @@ interface taskType{
 }
 export function Tasks(){
     const [tasks, setTask] = useState<taskType[]>([])
+    const [inputValue, setInputValue] = useState("")
 
     useEffect(() => {
         const fetchExpressData = async() => {
@@ -18,10 +19,13 @@ export function Tasks(){
         fetchExpressData()
     }, [])
 
-
     return(
         <div className="flex w-screen h-screen items-center justify-center">
             <div className="bg-zinc-600 w-7/12 min-h-11/12 h-fit rounded-lg p-3 gap-3 text-zinc-50 font-sans">
+            <div className="flex gap-3 w-full">
+                <input onChange={(e) => setInputValue(e.target.value)} placeholder="Insert task here..." className="flex-1 bg-zinc-800 w-full p-3 rounded-md min-h-10 max-h-fit focus-visible:ring-2 focus-visible:ring-violet-500 outline-none focus:scale-100.5 transition-all"></input>
+                {inputValue && <button onClick = {() => addTask(inputValue)}className="w-27 bg-purple-600 rounded-lg hover:brightness-90 p-3 font-bold hover: cursor-pointer hover:scale-105 transition-all ">ADD TASK</button>}
+            </div>
                 {tasks.map((task: taskType) => (
                     <div className="flex gap-3 items-center my-4">
                         <div className="w-25 text-zinc-50 text-center bg-purple-600 rounded-md p-5 text-3xl font-bold ">{task.id}</div>
@@ -33,9 +37,10 @@ export function Tasks(){
         </div>
     )
 
-    function deleteTask(task: taskType){
-        const updated = tasks.filter((t) => t.id !== task.id) //1 = 1 false so don't return 1, wtf is wrong?
-        setTask(updated)
-        console.log("Task: " + task.id)
+    async function addTask(input: string){{
+        await axios.post("http://localhost:3000/data/", { name: input })
+    }}
+    async function deleteTask(task: taskType){
+        await axios.delete(`http://localhost:3000/data/${task.id}`)
     }
 }
