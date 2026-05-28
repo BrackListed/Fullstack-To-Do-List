8ace9536ca06ca94eb77592bd23f57c5a7a1428f
BrackListed <cameta8900@gmail.com>
2026-05-28 17:37:13 +0800

    Displays post request with a delete button

--- backend/index.ts
+++ backend/index.ts
@@ -2,13 +2,20 @@ import express from "express"
 import cors from "cors"
 //npx nodemon --exec ts-node index.ts
 
+
+let tasks: any[] = []
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
 app.post('/data', (req, res) => {
-    res.json([req.body])
+    tasks.push(req.body)
+    res.json(tasks)
+})
+
+app.get("/data", (req, res) => {
+    res.json(tasks)
 })
 
 app.listen(3000, () => console.log("Listening on port 3000"))
--- frontend/src/pages/Tasks.tsx
+++ frontend/src/pages/Tasks.tsx
@@ -1,9 +1,6 @@
 import { useEffect, useState } from "react"
 
-
-
-
-interface taskType {
+interface taskType{
     name: string
     id: number;
 }
@@ -12,14 +9,7 @@ export function Tasks(){
 
     useEffect(() => {
         const fetchExpressData = async() => {
-        const response = await fetch("http://localhost:3000/data", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                name: "Jayce",
-                id: 1
-            })
-        })
+        const response = await fetch("http://localhost:3000/data")
         const temptask = await response.json()
         setTask(temptask)
     }
@@ -29,11 +19,21 @@ export function Tasks(){
 
     return(
         <div className="flex w-screen h-screen items-center justify-center">
-            <div className="bg-zinc-600 w-7/12 h-11/12 rounded-lg p-3 gap-3 text-zinc-50 font-sans">
+            <div className="bg-zinc-600 w-7/12 min-h-11/12 h-fit rounded-lg p-3 gap-3 text-zinc-50 font-sans">
                 {tasks.map((task: taskType) => (
-                    <li>{task.name}</li>
+                    <div className="flex gap-3 items-center my-4">
+                        <div className="w-25 text-zinc-50 text-center bg-purple-600 rounded-md p-5 text-3xl font-bold ">{task.id}</div>
+                        <div className="flex-1 text-zinc-50 bg-zinc-800 rounded-lg h-full p-5 text-2xl">{task.name}</div>
+                        <button onClick = {() => deleteTask(task)}className="w-30 text-slate-950 bg-red-600 p-5 text-2xl text-center font-bold rounded-md hover: cursor-pointer hover:brightness-90 hover:scale-105 transition-all  ">DELETE</button>
+                    </div>
                 ))}
             </div>
         </div>
     )
+
+    function deleteTask(task: taskType){
+        const updated = tasks.filter((t) => t.id !== task.id) //1 = 1 false so don't return 1, wtf is wrong?
+        setTask(updated)
+        console.log("Task: " + task.id)
+    }
 }
