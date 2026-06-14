fb01414e6ea1315a5e8c9359463d687f294902bc
BrackListed <cameta8900@gmail.com>
2026-06-14 15:50:42 +0800

    Watchlist header and placeholder values

--- frontend/src/pages/Watchlist.tsx
+++ frontend/src/pages/Watchlist.tsx
@@ -1,3 +1,4 @@
+import { Search } from "lucide-react"
 import { Left } from "../Components/Left"
 
 interface WatchlistProps{
@@ -12,7 +13,26 @@ export function Watchlist({setToggleSignIn, setToggleSignUp}: WatchlistProps){
             setToggleSignIn = {setToggleSignIn}
             setToggleSignUp = {setToggleSignUp}
             ></Left>
-            <div className="flex-1 flex"></div>
+            <div className="flex-1 flex flex-col text-zinc-50 px-10">
+                <div className="w-full h-1/12 flex justify-between py-10 items-center">
+                    <h1 className="text-3xl font-semibold">Watchlist</h1>
+                    <div className="flex w-full justify-end gap-2 items-center">
+                        <input className="bg-neutral-700 outline-none focus-visible:ring-2 focus-visible:ring-violet-500 p-3 rounded-lg w-4/12 h-fit"></input>
+                        <Search width={30} height={30}/>
+                    </div>
+                </div>
+                <div>
+                    <span className="font-semibold border-b-4 border-white w-fit">My Collection</span>
+                    <div className="border-b-2 border-white/10"></div>
+                </div>
+                <div className="mt-10 flex w-12/12 gap-5 flex-wrap justify-center">
+                    <div className="flex flex-col">
+                        <div className="w-51 h-70 border-white/10 border-2 bg-neutral-900 rounded-md"></div>
+                        <span className="font-semibold mt-3">Title</span>
+                        <span className="text-sm text-gray-500">Author/Director</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
